63aef4a22a4e0a88b6cfa331e2c6094738fd764f
Jakub Grajber <grajber.jakub@gmail.com>
2022-04-28 14:59:50 +0200

    Add basic Flight DAO with hibernate implementation

---
+++ src/main/java/com/jg/dev/flightManager/dao/FlightDAO.java
@@ -0,0 +1,10 @@
+package com.jg.dev.flightManager.dao;
+
+import com.jg.dev.flightManager.entities.Flight;
+
+import java.util.List;
+
+public interface FlightDAO {
+
+    public List<Flight> getAll();
+}
---
+++ src/main/java/com/jg/dev/flightManager/dao/FlightDAOHibernateImpl.java
@@ -0,0 +1,35 @@
+package com.jg.dev.flightManager.dao;
+
+
+import com.jg.dev.flightManager.entities.Flight;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+public class FlightDAOHibernateImpl implements FlightDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public FlightDAOHibernateImpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public List<Flight> getAll() {
+
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<Flight> theQuery = currentSession.createQuery("from FlightEntity", Flight.class);
+        List<Flight> result = theQuery.getResultList();
+
+        return result;
+    }
+}
