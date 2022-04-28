586eeceeacb868b64b99f2e05897429e9785491c
Jakub Grajber <grajber.jakub@gmail.com>
2022-04-28 12:28:23 +0200

    Initial commit

---
+++ src/main/java/com/jg/dev/flightManager/FlightManagerApplication.java
@@ -0,0 +1,13 @@
+package com.jg.dev.flightManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jg/dev/flightManager/FlightManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jg.dev.flightManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
