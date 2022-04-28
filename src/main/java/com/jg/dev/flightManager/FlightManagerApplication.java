1bebbcab0096cf19f733c25ef3a0c50aab416435
Jakub Grajber <grajber.jakub@gmail.com>
2022-04-28 12:45:48 +0200

    Set up the basic project config

--- src/main/java/com/jg/dev/flightManager/FlightManagerApplication.java
+++ src/main/java/com/jg/dev/flightManager/FlightManagerApplication.java
@@ -2,9 +2,16 @@ package com.jg.dev.flightManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlightManagerApplication {
+public class FlightManagerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringApplicationBuilder.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightManagerApplication.class, args);
